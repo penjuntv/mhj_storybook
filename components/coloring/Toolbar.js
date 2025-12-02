48978ef9d1da7742bb61fab78bda67d13694373f
penjuntv <penjunetv@gmail.com>
2025-12-02 18:03:39 +1300

    Update Toolbar.js

--- components/coloring/Toolbar.js
+++ components/coloring/Toolbar.js
@@ -1,17 +1,10 @@
 // components/coloring/Toolbar.js
-// 지금은 "앞으로 넣을 기능"의 자리를 잡는 수준의 툴바
-// 나중에: 장면별 캡처 저장, 되돌리기/다시하기, 선 두께 변경 등 추가 예정.
+// 앞으로 '저장하기', '되돌리기', 'AI 선그림 불러오기' 등을 붙일 툴바의 뼈대
 
 export default function Toolbar() {
   return (
     <div className="coloring-toolbar">
-      <button
-        type="button"
-        className="secondary-button"
-        onClick={() =>
-          alert("나중에 '장면별 그림 저장하기' 기능이 여기에 들어갈 예정입니다.")
-        }
-      >
+      <button type="button" className="toolbar-button" disabled>
         그림 저장하기 (준비 중)
       </button>
     </div>
