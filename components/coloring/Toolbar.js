a85e051331fee27417c1027a59ea920c621d49d3
penjuntv <penjunetv@gmail.com>
2025-12-02 13:08:39 +1300

    Update Toolbar.js

--- components/coloring/Toolbar.js
+++ components/coloring/Toolbar.js
@@ -1,5 +1,6 @@
 // components/coloring/Toolbar.js
-// 아직 기능은 최소한으로 두고, 나중에 "페이지 저장 / 불러오기" 등을 붙이기 위한 자리만 마련.
+// 지금은 "앞으로 넣을 기능"의 자리를 잡는 수준의 툴바
+// 나중에: 장면별 캡처 저장, 되돌리기/다시하기, 선 두께 변경 등 추가 예정.
 
 export default function Toolbar() {
   return (
@@ -7,7 +8,9 @@ export default function Toolbar() {
       <button
         type="button"
         className="secondary-button"
-        onClick={() => alert("나중에 '그림 저장하기' 기능을 여기에 붙일 예정입니다.")}
+        onClick={() =>
+          alert("나중에 '장면별 그림 저장하기' 기능이 여기에 들어갈 예정입니다.")
+        }
       >
         그림 저장하기 (준비 중)
       </button>
