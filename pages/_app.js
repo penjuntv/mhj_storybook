07c304e392b0eb4baa673829788e1219c405e8a0
penjuntv <penjunetv@gmail.com>
2025-12-02 18:18:31 +1300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 // pages/_app.js
-import "../styles/globals.css";
-import "../styles/coloring.css";
+// 전역 스타일만 불러오는 Next.js 커스텀 App 컴포넌트
 
-export default function MyApp({ Component, pageProps }) {
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
